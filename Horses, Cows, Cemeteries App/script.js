ccdd35ee2f1578bfc4213aca0e79ab6c0e319c86
ereynolds123 <51254660+ereynolds123@users.noreply.github.com>
2020-04-25 08:54:10 -0400

    Add files via upload
    The pseudo-code and beginnings of code for the HCC app. I also added boiler plate readline code and the code for the states. I need to figure a way to connect the state to the button on the app.  This is day 0 of the 100 day challenge.

---
+++ Horses, Cows, Cemeteries App/script.js
@@ -0,0 +1,63 @@
+/* All screens
+boiler plate code
+*/
+
+
+const readline = require('readline');
+const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+
+function ask(questionText) {
+  return new Promise((resolve, reject) => {
+    readlineInterface.question(questionText, resolve);
+  });
+}
+
+/* The screen order
+start --> teamName
+teamName--> gameState
+gameState--> congratulations || betterLuckNextTime
+congratulations --> start
+betterLuckNextTime --> start
+*/
+
+let states = {
+  "start": {canChangeTo: ["teamName"]},
+  "teamName": {canChangeTo: ["gameState"]},
+  "gameState": {canChangeTo: ["congratulations", "betterLuckNextTime"]},
+  "congratulations": {canChangeTo: ["start"]},
+  "betterLuckNextTime": {canChangeTo: ["start"]},
+}
+
+/* Potential code to switch states. Really, I need to connect the
+state to the clicked button.
+
+let currentState = "start";
+
+function enterState(newState) {
+  let validTransitions = states[currentState].canChangeTo;
+  if (validTransitions.includes(newState)) {
+    currentState = newState;
+  }
+*/
+
+
+/* teamName screen
+input teamName
+pick which side if in teamName state
+*/
+
+/* gameState screen
+let points for both teams start at 0
+if horse is clicked, add 10 points
+if cow is clicked, subtract 1 point
+if cemetery is clicked, subtract 100 points
+keep a running tally of points
+*/
+
+/*congratulations
+maybe a balloon animation feature eventually
+*/
+
+/*betterLuckNextTime
+maybe an animation eventually
+*/
