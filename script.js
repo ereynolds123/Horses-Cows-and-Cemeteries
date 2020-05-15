267d0ad36f2325f6243322a85a690376ff09cca9
ereynolds123 <51254660+ereynolds123@users.noreply.github.com>
2020-05-15 09:27:49 -0400

    Update script.js
    Added click functionality to the icons

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 
-/* All screens
-boiler plate code
-*/
-
-/*
-const readline = require('readline');
-const readlineInterface = readline.createInterface(process.stdin, process.stdout);
-
-function ask(questionText) {
-  return new Promise((resolve, reject) => {
-    readlineInterface.question(questionText, resolve);
-  });
-}
-*/
 
 /* The screen order
 start --> teamName
@@ -22,39 +8,56 @@ congratulations --> start
 betterLuckNextTime --> start
 */
 
-let states = {
-  "start": {canChangeTo: ["teamName"]},
-  "teamName": {canChangeTo: ["gameState"]},
-  "gameState": {canChangeTo: ["congratulations", "betterLuckNextTime"]},
-  "congratulations": {canChangeTo: ["start"]},
-  "betterLuckNextTime": {canChangeTo: ["start"]},
-}
-
-/* Potential code to switch states. Really, I need to connect the
-state to the clicked button.
-
-let currentState = "start";
-
-function enterState(newState) {
-  let validTransitions = states[currentState].canChangeTo;
-  if (validTransitions.includes(newState)) {
-    currentState = newState;
-  }
-*/
-
 
 /* teamName screen
 input teamName
 pick which side if in teamName state
 */
 
-/* gameState screen
+
+/* gameplay screen
 let points for both teams start at 0
 if horse is clicked, add 10 points
 if cow is clicked, subtract 1 point
 if cemetery is clicked, subtract 100 points
 keep a running tally of points
 */
+
+//This section clicks on the various buttons for Team One and adds to the point total Team One. 
+let scoreTeamOne = 
+  document.getElementById('horseTeamOne').addEventListener("click", clickedHorseTeamOne);
+
+  function clickedHorseTeamOne() {
+    console.log ("horse team one")
+  }
+  document.getElementById('cowTeamOne').addEventListener("click", clickedCowTeamOne);
+
+  function clickedCowTeamOne() {
+    console.log ("cow team one")
+  }
+  document.getElementById('cemeteryTeamOne').addEventListener("click", clickedCemeteryTeamOne);
+
+  function clickedCemeteryTeamOne() {
+    console.log ("cemetery team one")
+  }
+
+//This section clicks on the various buttons for Team Two and adds to the point total for Team Two. 
+let scoreTeamTwo = 
+  document.getElementById('horseTeamTwo').addEventListener("click", clickedHorseTeamTwo);
+
+  function clickedHorseTeamTwo() {
+    console.log ("horse team two")
+  }
+  document.getElementById('cowTeamTwo').addEventListener("click", clickedCowTeamTwo);
+
+  function clickedCowTeamTwo() {
+    console.log ("cow team two")
+  }
+  document.getElementById('cemeteryTeamTwo').addEventListener("click", clickedCemeteryTeamTwo);
+
+  function clickedCemeteryTeamTwo() {
+    console.log ("cemetery team two")
+  }
 
 /*congratulations
 maybe a balloon animation feature eventually
@@ -63,9 +66,3 @@ maybe a balloon animation feature eventually
 /*betterLuckNextTime
 maybe an animation eventually
 */
-
-function startGame(){
-  var teamName = document.forms["start-game"]["team-name"].value;
-  var sideOfCar=document.forms["start-game"]["side-of-car"].value;
-  alert("Your team name is "+ teamName + ". You are on the " + sideOfCar + " side of the car.");
-}
